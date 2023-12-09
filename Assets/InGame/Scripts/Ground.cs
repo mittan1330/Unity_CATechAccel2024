5ebdaab033e24dbf9d9ad6296b3157654355fea0
toyakazuya <mimikaumi@gmail.com>
2023-12-10 00:12:57 +0900

    URPとECSのパッケージいれた。 時間あったらECSの課題も入れとく。・・・・

--- Assets/InGame/Scripts/Ground.cs
+++ Assets/InGame/Scripts/Ground.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Entities.Graphics;
+using Unity.Rendering;
 
 namespace RunGame.FIeldGenerator
 {
     public class Ground : MonoBehaviour
     {
-
+        
+        
     }
 }
