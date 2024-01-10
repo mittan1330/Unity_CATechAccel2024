59055455a143b33c022a21cc72f0292959d63224
Shimon-Kuwahara <tukubamj@icloud.com>
2024-01-10 22:46:09 +0900

    sliderに名前をつける

---
+++ Assets/InGame/Scripts/Item/Item.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "Item", menuName = "CreateItem")]
+public class Item : ScriptableObject
+{
+    public enum Type // 実装するItemの種類
+    {
+        UserItem,
+        CraftItem,
+        KeyItem,
+    }
+
+    public Type type; // 種類
+    public String infomation; // 情報
+
+    public Item(Item item)
+    {
+        this.type = item.type;
+        this.infomation = item.infomation;
+    }
+}
