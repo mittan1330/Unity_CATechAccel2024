57292c70996ab11f26c4f83b8d96f06dd620c26d
Shimon-Kuwahara <tukubamj@icloud.com>
2024-01-12 12:28:12 +0900

    命名規則
    特に治せるところを見つけられなかったので、今後の実装で意識していきたい

--- Assets/OutGame/Scripts/ScoreManager.cs
+++ Assets/OutGame/Scripts/ScoreManager.cs
@@ -19,17 +19,11 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < rankCells.Length; i++)
+        for (int i = 0; i < rankCells.Length; i++)
         {
             rankCells[i].userName.text = userScoreData[i].name;
             rankCells[i].score.text = userScoreData[i].score;
             rankCells[i].rank.text = userScoreData[i].rank;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
