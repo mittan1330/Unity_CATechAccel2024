98b7a861a8a0800d6adb24b6cb4cf6dcb5b1db44
Shimon-Kuwahara <tukubamj@icloud.com>
2024-02-03 20:36:39 +0900

    未実装

--- Assets/OutGame/Scripts/ScoreManagerView.cs
+++ Assets/OutGame/Scripts/ScoreManagerView.cs
@@ -1,9 +1,24 @@
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class RankingPanelView : MonoBehaviour
 {
     [SerializeField] private Transform contentTransform;
     [SerializeField] private GameObject rankCellPrefab;
+
+    private ObjectPool<GameObject> pool;
+
+    private void Start()
+    {
+        ObjectPool<GameObject> pool = new ObjectPool<GameObject>(
+        createFunc: () => GameObject.CreatePrimitive(PrimitiveType.Cube),         // プールが空のときに新しいインスタンスを生成する処理
+        actionOnGet: target => target.SetActive(true),                            // プールから取り出されたときの処理 
+        actionOnRelease: target => target.SetActive(false),                       // プールに戻したときの処理
+        actionOnDestroy: target => Destroy(target),                               // プールがmaxSizeを超えたときの処理
+        collectionCheck: true,                                                    // 同一インスタンスが登録されていないかチェックするかどうか
+        defaultCapacity: 10,                                                      // デフォルトの容量
+        maxSize: 100);
+    }
 
     public void UpdateUIWithUserData(ScoreData scoreData)
     {
