ffe14dcc2d278a79b53af8308faa5d37236ab207
Shimon-Kuwahara <tukubamj@icloud.com>
2024-02-14 15:51:32 +0900

    動画撮影のために修正

--- Assets/OutGame/Scripts/RankingPanelView.cs
+++ Assets/OutGame/Scripts/RankingPanelView.cs
@@ -11,12 +11,12 @@ public class RankingPanelView : MonoBehaviour
     private void Start()
     {
         ObjectPool<GameObject> pool = new ObjectPool<GameObject>(
-        createFunc: () => GameObject.CreatePrimitive(PrimitiveType.Cube),         // プールが空のときに新しいインスタンスを生成する処理
-        actionOnGet: target => target.SetActive(true),                            // プールから取り出されたときの処理 
-        actionOnRelease: target => target.SetActive(false),                       // プールに戻したときの処理
-        actionOnDestroy: target => Destroy(target),                               // プールがmaxSizeを超えたときの処理
-        collectionCheck: true,                                                    // 同一インスタンスが登録されていないかチェックするかどうか
-        defaultCapacity: 10,                                                      // デフォルトの容量
+        createFunc: () => GameObject.CreatePrimitive(PrimitiveType.Cube),   
+        actionOnGet: target => target.SetActive(true),                            
+        actionOnRelease: target => target.SetActive(false),                       
+        actionOnDestroy: target => Destroy(target),                               
+        collectionCheck: true,                                                    
+        defaultCapacity: 10,                                                      
         maxSize: 100);
     }
 
